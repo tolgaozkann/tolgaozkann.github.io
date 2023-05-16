41487758a7b638dec502d4b0052a25fb9b7095bc
tolgaozkann <ozkanozkan3233@gmail.com>
2023-05-16 23:48:35 +0300

    activity8 added

--- inclassactivities/js/Activity8.js
+++ inclassactivities/js/Activity8.js
@@ -1,2 +1,29 @@
+var languagesList = [
+    "ActionScript",
+    "AppleScript",
+    "Asp",
+    "JavaScript",
+    "Lisp",
+    "Perl",
+    "Php",
+    "Python",
+    "C#",
+    "Java",
+    "C",
+    "C++"
+]
+
+
+$(document).ready(function(){
+    $('#birthday').datepicker();
+
+    
+});
+
+$(document).ready(function(){
+    $("#programmingLanguage").autocomplete({
+        source:languagesList
+    });
+});
 
 
