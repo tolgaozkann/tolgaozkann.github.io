5dff3c38a6a5417f9fb546c5305fb266f09b907c
tolgaozkann <ozkanozkan3233@gmail.com>
2023-06-11 16:06:52 +0300

    code refactoring

--- project/js/aboutus.js
+++ project/js/aboutus.js
@@ -1,23 +1,3 @@
-// $(document).ready(function() {
-
-//     $("#owl-demo").owlCarousel({
-
-//     navigation : true, // Show next and prev buttons
-
-//     slideSpeed : 300,
-//     paginationSpeed : 400,
-
-//     items : 1, 
-//     itemsDesktop : false,
-//     itemsDesktopSmall : false,
-//     itemsTablet: false,
-//     itemsMobile : false
-
-//     });
-
-//     });
-
-
 
 //fancy messenger plugin 
 $(document).ready(function () {
